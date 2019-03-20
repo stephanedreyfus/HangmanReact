e7c88f555795e30f1163ec068abdce0f3a85ab81
annika <annikaslund@gmail.com>
2019-03-20 12:16:53 -0700

    Add tests for images

--- src/Hangman.test.js
+++ src/Hangman.test.js
@@ -12,3 +12,18 @@ it("matches snapshot", function() {
     let serialized = toJson(wrapper);
     expect(serialized).toMatchSnapshot();
 });
+
+it("changes images when an incorrect guess is made", function() {
+  let wrapper = shallow(<Hangman />);
+  wrapper.find("button[value='b']").simulate("click", { target: { value: "b" } });
+  let imgSrc = wrapper.find('img').first().html()
+  expect(imgSrc).not.toContain('0.jpg');
+});
+
+it("does not change image when a correct guess is made", function() {
+  let wrapper = shallow(<Hangman />);
+  let imgSrc = wrapper.find('img').first().html()
+  expect(imgSrc).toContain('0.jpg');
+  wrapper.find("button[value='a']").simulate("click", { target: { value: "a" } });
+  expect(imgSrc).toContain('0.jpg');
+});
